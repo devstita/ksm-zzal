a284c10d800e7e393cb39d5566ebe72731f55913
Devstita <devstita@gmail.com>
2021-12-02 11:39:18 +0900

    Completed image processing

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
 from PIL import Image, ImageFont, ImageDraw
 
+def tuple_elementwise_calc(t1, t2, op):
+    return tuple([op(x, y) for x, y in zip(t1, t2)])
+
 def get_newlined_text(m, draw, font, max_width): 
     m_edit = m[:]
     while True: 
         text_width = draw.textsize(m_edit, font=font)[0]
-        print(m_edit)
+        # print(m_edit)
         if text_width > max_width - 200:
             m_edit = m_edit[:-1]
         else:
@@ -19,18 +22,17 @@ def get_newlined_text(m, draw, font, max_width):
 def create_image(m1, m2): 
     image = Image.open('templates/res/sungmo_origin.png')
     font = ImageFont.truetype('templates/res/NanumGothicBold.ttf', 60)
-    width = image.size[0]
+    image_width = image.size[0]
     draw = ImageDraw.Draw(image)
 
-    m1 = get_newlined_text(m1, draw, font, width)
-    m2 = get_newlined_text(m2, draw, font, width)
+    m1 = get_newlined_text(m1, draw, font, image_width)
+    m2 = get_newlined_text(m2, draw, font, image_width)
 
-    print(m1)
-    print('====')
-    print(m2)
-    
-    draw.text(((width - draw.textsize(m1, font=font)[0]) / 2, 140), m1, font=font, fill='#000000')
-    draw.text(((width - draw.textsize(m2, font=font)[0]) / 2, 400), m2, font=font, fill='#000000')
+    sp_middle_coord = [(320.0, 157.5), (320.0, 825.0)]
+    m_middle_coord = [tuple([x / 2 for x in draw.textsize(m1, font=font)]), tuple([x / 2 for x in draw.textsize(m2, font=font)])]
+
+    draw.text(tuple_elementwise_calc(sp_middle_coord[0], m_middle_coord[0], lambda a, b: a - b), m1, font=font, fill='#000000')
+    draw.text(tuple_elementwise_calc(sp_middle_coord[1], m_middle_coord[1], lambda a, b: a - b), m2, font=font, fill='#000000')
     image.show()
 
-create_image('XX가...', '말대꾸대대대대꾸대꾸꾸꾸?!')
+create_image('지명이가...', '말대꾸?!')
