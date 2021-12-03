02a1f8b55278bfeac1bae5fed053594fe99becc6
Devstita <devstita@gmail.com>
2021-12-03 10:28:08 +0900

    developed image processing feature with automatic height adjustment

--- process.py
+++ process.py
@@ -3,7 +3,7 @@ from PIL import Image, ImageFont, ImageDraw
 def tuple_elementwise_calc(t1, t2, op):
     return tuple([op(x, y) for x, y in zip(t1, t2)])
 
-def get_newlined_text(m, draw, font, max_width): 
+def get_newlined_text_list(m, draw, font, max_width): 
     m_edit = m[:]
     while True: 
         text_width = draw.textsize(m_edit, font=font)[0]
@@ -14,9 +14,9 @@ def get_newlined_text(m, draw, font, max_width):
             break
     
     if len(m_edit) == len(m): 
-        return m
+        return [m]
     else:
-        return m_edit + '\n' + m[len(m_edit):]
+        return [m_edit, *get_newlined_text_list(m[len(m_edit):], draw, font, max_width)]
         
 
 def create_image(m1, m2): 
@@ -25,16 +25,20 @@ def create_image(m1, m2):
     image_width = image.size[0]
     draw = ImageDraw.Draw(image)
 
-    m1 = get_newlined_text(m1, draw, font, image_width)
-    m2 = get_newlined_text(m2, draw, font, image_width)
+    m = [get_newlined_text_list(m1, draw, font, image_width), get_newlined_text_list(m2, draw, font, image_width)]
+    sp_middle_coord = [(320.0, 155.0), (320.0, 820.0)]
 
-    sp_middle_coord = [(320.0, 157.5), (320.0, 825.0)]
-    m_middle_coord = [tuple([x / 2 for x in draw.textsize(m1, font=font)]), tuple([x / 2 for x in draw.textsize(m2, font=font)])]
-
-    draw.text(tuple_elementwise_calc(sp_middle_coord[0], m_middle_coord[0], lambda a, b: a - b), m1, font=font, fill='#000000')
-    draw.text(tuple_elementwise_calc(sp_middle_coord[1], m_middle_coord[1], lambda a, b: a - b), m2, font=font, fill='#000000')
+    for i in range(2): 
+        m_middle_coord = [tuple([x / 2 for x in draw.textsize(cur, font=font)]) for cur in m[i]]
+        pos = tuple_elementwise_calc(sp_middle_coord[i], m_middle_coord[0], lambda a, b: a - b)
+        pos = (pos[0], pos[1] - (draw.textsize('\n'.join(m[i]), font=font)[1] - draw.textsize(m[i][0], font=font)[1]) / 2)
+        
+        draw.text(pos, m[i][0], font=font, fill='#000000')
+        for idx, line in enumerate(m[i][1:], 1): 
+            pos = (tuple_elementwise_calc(sp_middle_coord[i], m_middle_coord[idx], lambda a, b: a - b)[0], pos[1] + draw.textsize(line, font=font)[1])
+            draw.text(pos, m[i][idx], font=font, fill='#000000')
     
     return image
 
 if __name__ == '__main__':
-    create_image('지명이가...', '말대꾸?!')
+    create_image('사랑헤이요...', '연애가중계...').show()
